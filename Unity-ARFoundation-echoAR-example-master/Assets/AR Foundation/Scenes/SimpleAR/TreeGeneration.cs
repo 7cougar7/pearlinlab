e7917c15d57f850b3501c7d69eb2dc7d825e372f
Atilano (Tilo) Garcia <tilogarcia1@gmail.com>
2020-10-24 22:22:32 -0500

    New potential solution?

---
+++ Unity-ARFoundation-echoAR-example-master/Assets/AR Foundation/Scenes/SimpleAR/TreeGeneration.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGeneration : MonoBehaviour
+{
+
+	[SerializeField]
+	private NoiseMapGeneration noiseMapGeneration;
+
+	[SerializeField]
+	private Wave[] waves;
+
+	[SerializeField]
+	private float levelScale;
+
+	[SerializeField]
+	private float[] neighborRadius;
+
+	[SerializeField]
+	private GameObject[] treePrefab;
+
+	public void GenerateTrees(int levelDepth, int levelWidth, float distanceBetweenVertices, LevelData levelData)
+	{
+		// generate a tree noise map using Perlin Noise
+		float[,] treeMap = this.noiseMapGeneration.GeneratePerlinNoiseMap(levelDepth, levelWidth, this.levelScale, 0, 0, this.waves);
+
+		float levelSizeX = levelWidth * distanceBetweenVertices;
+		float levelSizeZ = levelDepth * distanceBetweenVertices;
+
+		for (int zIndex = 0; zIndex < levelDepth; zIndex++)
+		{
+			for (int xIndex = 0; xIndex < levelWidth; xIndex++)
+			{
+				// convert from Level Coordinate System to Tile Coordinate System and retrieve the corresponding TileData
+				TileCoordinate tileCoordinate = levelData.ConvertToTileCoordinate(zIndex, xIndex);
+				TileData tileData = levelData.tilesData[tileCoordinate.tileZIndex, tileCoordinate.tileXIndex];
+				int tileWidth = tileData.heightMap.GetLength(1);
+
+				// calculate the mesh vertex index
+				Vector3[] meshVertices = tileData.mesh.vertices;
+				int vertexIndex = tileCoordinate.coordinateZIndex * tileWidth + tileCoordinate.coordinateXIndex;
+
+				// get the terrain type of this coordinate
+				TerrainType terrainType = tileData.chosenHeightTerrainTypes[tileCoordinate.coordinateZIndex, tileCoordinate.coordinateXIndex];
+
+				// get the biome of this coordinate
+				Biome biome = tileData.chosenBiomes[tileCoordinate.coordinateZIndex, tileCoordinate.coordinateXIndex];
+				float maxIndexf = Mathf.Min((this.neighborRadius.Length - 1), 99999); //TODO Fix this eventually i guess
+				int maxIndex = (int)maxIndexf;
+				// check if it is a water terrain. Trees cannot be placed over the water
+				if (terrainType.name != "water" && maxIndex >= 0)
+				{
+					float treeValue = treeMap[zIndex, xIndex];
+
+					int terrainTypeIndex = terrainType.index;
+
+					Debug.Log("Biome I: " + biome.index);
+					Debug.Log("this.neighborRadius length: " + this.neighborRadius.Length);
+					//Debug.Log(zIndex - this.neighborRadius[biome.index]);
+
+					// compares the current tree noise value to the neighbor ones
+					int neighborZBegin = (int)Mathf.Max(0, zIndex - this.neighborRadius[maxIndex]);
+					int neighborZEnd = (int)Mathf.Min(levelDepth - 1, zIndex + this.neighborRadius[maxIndex]);
+					int neighborXBegin = (int)Mathf.Max(0, xIndex - this.neighborRadius[maxIndex]);
+					int neighborXEnd = (int)Mathf.Min(levelWidth - 1, xIndex + this.neighborRadius[maxIndex]);
+					float maxValue = 0f;
+					for (int neighborZ = neighborZBegin; neighborZ <= neighborZEnd; neighborZ++)
+					{
+						for (int neighborX = neighborXBegin; neighborX <= neighborXEnd; neighborX++)
+						{
+							float neighborValue = treeMap[neighborZ, neighborX];
+							// saves the maximum tree noise value in the radius
+							if (neighborValue >= maxValue)
+							{
+								maxValue = neighborValue;
+							}
+						}
+					}
+
+					// if the current tree noise value is the maximum one, place a tree in this location
+					if (treeValue == maxValue)
+					{
+						Vector3 treePosition = new Vector3(xIndex * distanceBetweenVertices, meshVertices[vertexIndex].y, zIndex * distanceBetweenVertices);
+						int maxTreeFab = (int)Mathf.Min(this.treePrefab.Length - 1, biome.index);
+						GameObject tree = Instantiate(this.treePrefab[maxTreeFab], treePosition, Quaternion.identity) as GameObject;
+						tree.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+					}
+				}
+			}
+		}
+	}
+}
+
---
+++ Unity-ARFoundation-echoAR-example-master/Assets/echoAR/CustomBehaviour.cs
@@ -0,0 +1,64 @@
+﻿/**************************************************************************
+* Copyright (C) echoAR, Inc. 2018-2020.                                   *
+* echoAR, Inc. proprietary and confidential.                              *
+*                                                                         *
+* Use subject to the terms of the Terms of Service available at           *
+* https://www.echoar.xyz/terms, or another agreement                      *
+* between echoAR, Inc. and you, your company or other organization.       *
+***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System;
+using UnityEngine;
+
+
+public class CustomBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public Entry entry;
+    string ECHOAR_API_KEY = "cool-fire-1148";
+
+
+    /// <summary>
+    /// EXAMPLE BEHAVIOUR
+    /// Queries the database and names the object based on the result.
+    /// </summary>
+
+    // Use this for initialization
+    void Start()
+    {
+        // Add RemoteTransformations script to object and set its entry
+        this.gameObject.AddComponent<RemoteTransformations>().entry = entry;
+
+        // Qurey additional data to get the name
+        string value = "";
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("name", out value))
+        {
+            // Set name
+            this.gameObject.name = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) {
+            string direction = "";
+            if (entry.getAdditionalData().TryGetValue("direction", out direction)){
+                string newDirection = direction == "right" ? "left" : "right"; 
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://console.echoAR.xyz/post?key={0}&entry={1}&data={2}&value={3}", ECHOAR_API_KEY, entry.getId(), "direction", newDirection));
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            }
+        // Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+        // RaycastHit hit;
+        // if(Physics.Raycast(ray, out hit)) {
+        //     if(hit.collider != null)
+        //     {
+        //         Color newColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0);
+        //         hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+        //     }
+        }
+      
+    }
+}
